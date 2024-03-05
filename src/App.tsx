3f23df2a7875cd7f3f5bf24a6de358845c81c120
AbdulrahmanIsmailMohamed <abdulrahman.ismail.mohammed@gmail.com>
2024-03-05 22:10:20 +0200

    container

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="font color-body">
+      <Container className="py-5">
+        <Row className="justify-content-center">
+          <Col sm="8" className="">
+            لديك اربع مواعيد اليوم:
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col sm="8" className="">
+            <div className="rectangle"></div>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col sm="8" className="d-flex justify-content-between">
+            <Button className="btn-style p-2">مسح الكل</Button>
+            <Button className="btn-style p-2">عرض البيانات</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default App
+export default App;
